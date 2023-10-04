78539fd38da8758783caa81b689bca580c1e2c1c
Daniel Meraz <dmg@Daniels-MacBook-Pro.local>
2023-10-04 10:05:51 -0700

    cofigured db azure postgres sql and running

--- Alexandria/tests.py
+++ Alexandria/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+import os 
 
-# Create your tests here.
+print("Database Name:", os.getenv('DB_NAME'))
+print("Database Host:", os.getenv('DB_HOST'))
+print("Database User:", os.getenv('DB_USER'))
+print("Database Password:", os.getenv('DB_PASSWORD'))
--- odin/settings.py
+++ odin/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Alexandria',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,16 @@ WSGI_APPLICATION = 'odin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'laborales',
+        'HOST': 'jiistdb.postgres.database.azure.com',
+        'PORT': '5432',
+        'USER': 'dmg',
+        'PASSWORD': 'hurduq-tydbut-5jewQe',
+        'OPTIONS': {'sslmode': 'require'},
+        },
 }
+
 
 
 # Password validation
